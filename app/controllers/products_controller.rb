86ab38721a0ba1dd37e3d42f3104601a6ab57378
Luis Aguilera <aguilera.glz.luis@gmail.com>
2017-05-29 20:10:56 -0500

    product payments

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,119 @@
+include PayPal::SDK::REST
+
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  protect_from_forgery with: :null_session
+
+  # GET /products
+  # GET /products.json
+  def index
+    @products = Product.all
+  end
+
+  # GET /products/1
+  # GET /products/1.json
+  def show
+  end
+
+  # GET /products/new
+  def new
+    @product = Product.new
+  end
+
+  # GET /products/1/edit
+  def edit
+  end
+
+  # POST /products
+  # POST /products.json
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /products/1
+  # PATCH/PUT /products/1.json
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /products/1
+  # DELETE /products/1.json
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def buy
+    @product = Product.find(params[:id])
+    # Build Payment object
+    @payment = Payment.new({
+      :intent => "sale",
+      :payer => {
+        :payment_method => "credit_card",
+        :funding_instruments => [{
+          :credit_card => {
+            :type => "visa",
+            :number => "4567516310777851",
+            :expire_month => "11",
+            :expire_year => "2018",
+            :cvv2 => "874",
+            :first_name => "Joe",
+            :last_name => "Shopper",
+            :billing_address => {
+              :line1 => "52 N Main ST",
+              :city => "Johnstown",
+              :state => "OH",
+              :postal_code => "43210",
+              :country_code => "US" }}}]},
+      :transactions => [{
+        :item_list => {
+          :items => [{
+            :name => "item",
+            :sku => "item",
+            :price => "1",
+            :currency => "USD",
+            :quantity => 1 }]},
+        :amount => {
+          :total => "1.00",
+          :currency => "USD" },
+        :description => "This is the payment transaction description." }]})
+    if @payment.create
+      # Redirect the user to given approval url
+      redirect_to action: "show", id: @product.id
+    else
+      render json: @payment.error
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:nam, :price)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  resources :products
   root 'welcome#index'
 
   get 'welcome/index'
 
   resources :articles do
     resources :comments
+  end
+
+  resources :products do
+    member do
+      get 'buy'
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
